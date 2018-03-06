ce07c42d47f58d16cd986df947d011753cecc418
Mrrvm <mrrvm@hotmail.com>
2018-03-06 18:11:30 +0000

    Arguments dones

--- Project/client.c
+++ Project/client.c
@@ -1,37 +1,54 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_PORT 59000
 
 int main(int argc, char *argv[])
 {
-  int opt;
-  int port = -1;
-  char *ip = NULL;
+    int opt;
+    int port = -1;
+    char *ip = NULL;
+    struct hostent *hostptr;
 
-  while ((opt = getopt(argc, argv, "i:p:")) != -1) {
-      switch (opt) {
-        case 'i':
-            ip = optarg;
-            break;
-        case 'p':
-            port = atoi(optarg);
-            break;
-        default: /* '?' */
-            fprintf(stderr, "Usage: %s [-i csip] [-p csp]\n", argv[0]);
+    while ((opt = getopt(argc, argv, "i:p:")) != -1) {
+        switch (opt) {
+            case 'i':
+                ip = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            default: /* '?' */
+                fprintf(stderr, "Usage: %s [-i csip] [-p cspt]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if(ip == NULL) {
+        if(argc > 1) {
+            fprintf(stderr, "Usage: %s [-i csip] [-p cspt]\n", argv[0]);
             exit(EXIT_FAILURE);
-      }
-  }
+        }
+        hostptr = gethostbyname("tejo.tecnico.ulisboa.pt");
+        if(hostptr==NULL){exit(EXIT_FAILURE);}
+        printf("%s\n", inet_ntoa((hostptr->h_addr_list[0])));
+        //strcpy(ip);
+    }
 
-  if(ip == NULL || port == -1) {
-      fprintf(stderr, "Usage: %s [-i csip] [-p csp]\n", argv[0]);
-      exit(EXIT_FAILURE);
-  }
-  
-  if (argc != 5) {
-      fprintf(stderr, "Usage: %s [-i csip] [-p csp]\n", argv[0]);
-      exit(EXIT_FAILURE);
-  }
+    if(port == -1) {
+        if(argc > 1) {
+            fprintf(stderr, "Usage: %s [-i csip] [-p cspt]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        port = DEFAULT_PORT;
+    }
 
-   fprintf(stderr, "Arguments %s %d\n",ip, port );
-   exit(EXIT_SUCCESS);
+    fprintf(stderr, "Arguments %s %d\n",ip, port );
+    exit(EXIT_SUCCESS);
 }
