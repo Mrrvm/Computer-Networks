21f937f6ac0d5a8b4bcaca9b810a0fcd2b2e9f01
Mrrvm <mrrvm@hotmail.com>
2018-03-06 18:31:25 +0000

    It prints!

--- Project/client.c
+++ Project/client.c
@@ -13,13 +13,15 @@ int main(int argc, char *argv[])
 {
     int opt;
     int port = -1;
-    char *ip = NULL;
-    struct hostent *hostptr;
+    int sock;
+    int addrlen;
+    struct hostent *hostptr = NULL;
+    struct sockaddr_in serveraddr;
 
     while ((opt = getopt(argc, argv, "i:p:")) != -1) {
         switch (opt) {
             case 'i':
-                ip = optarg;
+                hostptr = gethostbyname(optarg);
                 break;
             case 'p':
                 port = atoi(optarg);
@@ -30,25 +32,26 @@ int main(int argc, char *argv[])
         }
     }
 
-    if(ip == NULL) {
-        if(argc > 1) {
-            fprintf(stderr, "Usage: %s [-i csip] [-p cspt]\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
+    if(hostptr == NULL) {
         hostptr = gethostbyname("tejo.tecnico.ulisboa.pt");
-        if(hostptr==NULL){exit(EXIT_FAILURE);}
-        printf("%s\n", inet_ntoa((hostptr->h_addr_list[0])));
-        //strcpy(ip);
+        if(hostptr==NULL) exit(EXIT_FAILURE);
     }
 
     if(port == -1) {
-        if(argc > 1) {
-            fprintf(stderr, "Usage: %s [-i csip] [-p cspt]\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
         port = DEFAULT_PORT;
     }
 
-    fprintf(stderr, "Arguments %s %d\n",ip, port );
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sock ==-1) exit(EXIT_FAILURE);
+    if(memset((void*)&serveraddr, (int)'\0', sizeof(serveraddr))==NULL) exit(EXIT_FAILURE);
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = ((struct in_addr*)(hostptr->h_addr_list[0]))->s_addr;
+    serveraddr.sin_port = htons((u_short)port);
+    addrlen = sizeof(serveraddr);
+
+
+    fprintf(stderr, "Arguments %s, %d\n", inet_ntoa((struct in_addr)serveraddr.sin_addr), port);
+
+    close(sock);
     exit(EXIT_SUCCESS);
 }
